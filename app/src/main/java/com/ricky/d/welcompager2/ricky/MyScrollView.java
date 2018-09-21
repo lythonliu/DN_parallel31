8e1af6a8e574b5bc9fb359fc6d897a72286706ba
Lython <liu.junli@moxiangroup.com>
2018-09-21 14:42:14 +0800

    init

---
+++ app/src/main/java/com/ricky/d/welcompager2/ricky/MyScrollView.java
@@ -0,0 +1,29 @@
+package com.ricky.d.welcompager2.ricky;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.HorizontalScrollView;
+import android.widget.ScrollView;
+
+public class MyScrollView extends HorizontalScrollView {
+
+	public MyScrollView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent ev) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent ev) {
+		// TODO Auto-generated method stub
+		//return true;
+		return false;
+	}
+
+}
